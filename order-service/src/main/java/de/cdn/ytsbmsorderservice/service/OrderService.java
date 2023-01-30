5e626c836c21ff9acef885c3d74bb071c9e6482d
xcx2309 <canberk.demirkan.extern@atruvia.de>
2023-01-30 23:59:24 +0100

    feat: add webflux, handle multiple skuCodes, inter service communication

--- order-service/src/main/java/de/cdn/ytsbmsorderservice/service/OrderService.java
+++ order-service/src/main/java/de/cdn/ytsbmsorderservice/service/OrderService.java
@@ -1,9 +1,12 @@
 package de.cdn.ytsbmsorderservice.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import de.cdn.ytsbmsorderservice.dto.InventoryResponse;
 import de.cdn.ytsbmsorderservice.dto.OrderLineItemsDto;
 import de.cdn.ytsbmsorderservice.dto.OrderRequest;
 import de.cdn.ytsbmsorderservice.model.Order;
@@ -16,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -26,7 +30,25 @@ public class OrderService {
                 .toList();
         order.setOrderLineItems(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItems().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        assert inventoryResponseArray != null;
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product out of stock.");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
